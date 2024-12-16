0ef5e4a0389f8e1bdc651ce29e512c867319d064
techypankaj <32155045+techypankaj@users.noreply.github.com>
2024-12-16 13:13:43 +0530

    Update omni_1917.mk
    hjh

--- omni_1917.mk
+++ omni_1917.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from 1917 device
 $(call inherit-product, device/vivo/1917/device.mk)
 
 PRODUCT_DEVICE := 1917
-PRODUCT_NAME := omni_1917
+PRODUCT_NAME := twrp_1917
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := vivo 1917
 PRODUCT_MANUFACTURER := vivo
